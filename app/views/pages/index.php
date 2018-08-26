04bdebdf915d514d66dc99c7c1556345bfa4e8eb
LaQuita <lgollman@wayne.edu>
2018-08-25 21:12:06 -0700

    framework files

---
+++ app/views/pages/index.php
@@ -0,0 +1,4 @@
+<?php require APPROOT . '/views/inc/header.php';?>
+    <h1><?php echo $data['title'] ?></h1>
+    <p>This is the CodesparkMVC PHP framework. Please refer to the docs for usage and implementation directions.</p>
+<?php require APPROOT . '/views/inc/footer.php';?>
